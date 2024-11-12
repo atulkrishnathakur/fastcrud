caba596e5a9bf0acb8db430fd9c1e61bfc58861e
Atul <atul77.cs@gmail.com>
2024-11-12 11:04:42 +0530

    insert users

--- database/model_functions/user.py
+++ database/model_functions/user.py
@@ -2,6 +2,7 @@ from database.model.user import User
 from fastapi import Depends
 from fastapi import status
 from sqlalchemy import select
+from sqlalchemy import insert
 from database.dbconnection import engine
 def read_all_user(db):
     try:
@@ -32,6 +33,41 @@ def read_all_user(db):
         result = db.execute(stmt)
         return result.scalars().all()
 
-
     except Exception as e:
         print(f"Exception error{e}")
+
+def saveUser(db):
+    try:
+        # https://docs.sqlalchemy.org/en/20/orm/queryguide/dml.html
+        
+        """
+        db.execute(
+            insert(User),
+            [
+                {"id":5,"firstname":"Ram","secondname":"Thakur","email":"myeml3@yopmail.com","status":1},
+                {"id":6,"firstname":"Balram","secondname":"Thakur","email":"myeml4@yopmail.com","status":1},
+                {"id":7,"firstname":"Krishna","secondname":"Thakur","email":"myeml5@yopmail.com","status":1},
+                {"id":8,"firstname":"Guru","secondname":"Thakur","email":"myeml6@yopmail.com","status":1}
+            ]
+        )
+        db.commit()
+        result = db.execute(select(User).order_by(User.id))
+        return result.scalars().all()
+        """
+        
+        """
+        userdata = db.execute(
+            insert(User).returning(User),
+            [
+                {"id":25,"firstname":"Ram","secondname":"Thakur","email":"myeml23@yopmail.com","status":1},
+                {"id":26,"firstname":"Balram","secondname":"Thakur","email":"myeml24@yopmail.com","status":1},
+                {"id":27,"firstname":"Krishna","secondname":"Thakur","email":"myeml25@yopmail.com","status":1},
+                {"id":28,"firstname":"Guru","secondname":"Thakur","email":"myeml26@yopmail.com","status":1}
+            ]
+        )
+        db.commit()
+        return userdata.scalars().all()
+        # returning() function return latest inserted users
+        """
+    except Exception as e:
+        print(f"Exception erro{e}")
--- router/api/user_route.py
+++ router/api/user_route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,Depends,status
 from typing import Annotated
 from sqlalchemy.orm import Session
 from database.session import get_db
-from database.model_functions.user import read_all_user
+from database.model_functions.user import read_all_user,saveUser
 
 router = APIRouter()
 
@@ -10,6 +10,14 @@ router = APIRouter()
 def getUser(db:Session = Depends(get_db)):
     try:
         allUser = read_all_user(db)
+        return allUser
+    except Exception as e:
+        print(f"Exception error {e}")
+
+@router.post("/create-user",name="createuser")
+def createUser(db:Session = Depends(get_db)):
+    try:
+        allUser = saveUser(db)
         return allUser
     except Exception as e:
         print(f"Exception error {e}")
